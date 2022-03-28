32751083c5c0a0f6533d7e514968c988169e60a4
Divyanshu Negi <divyanshunegi.dn@gmail.com>
2022-03-28 10:53:52 +0530

    chore: new endpoints added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 const landingZoneRouter = require('./routes/landingZone');
+const pathFindRouter = require('./routes/pathFind');
+const bfsRouter = require('./routes/bfs');
+const knapSackRouter = require('./routes/knapsack');
 
 app.use('/landingZone', landingZoneRouter);
+app.use('/pathFind', pathFindRouter);
+app.use('/bfs', bfsRouter);
+app.use('/knapSack', knapSackRouter);
 
 app.listen(3000, function () {
   console.log('Test app listening on port 3000!');
